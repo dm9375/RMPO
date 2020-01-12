ed40cbd8f1e60257b478e08b367aebd45b76c2a3
Daniel Mironiuk <dm9375@rit.edu>
2020-01-12 14:16:08 -0500

    First commit

---
+++ Course.js
@@ -0,0 +1,12 @@
+var courseId;
+var courseTime;
+var courseDays;
+var courseProf;
+class Course {
+    constructor(courseId, courseTime, courseDays, courseProf) {
+        this.courseId = courseId;
+        this.courseTime = courseTime;
+        this.courseDays = courseDays;
+        this.courseProf = courseProf;
+    }
+}
---
+++ Professor.js
@@ -0,0 +1,11 @@
+class Professor {
+    var rating;
+    var professorName;
+    var coursesTaught;
+    constructor(professorName, rating, coursesTaught) {
+        this.professorName = professorName;
+        this.rating = rating;
+        this.coursesTaught = coursesTaught;
+    }
+
+}
